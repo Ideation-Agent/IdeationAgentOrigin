4e1998c42e685d0096d5f4320566d073686d7285
Jongwon Park <p1231pjw@gmail.com>
2023-10-16 21:51:59 +0900

    researcher agent bugfix

--- autogpts/IdeationAgent/forge/agent.py
+++ autogpts/IdeationAgent/forge/agent.py
@@ -12,18 +12,8 @@ from forge.sdk import (
 )
 import os
 from dotenv import load_dotenv
-from langchain.agents import initialize_agent, Tool
-from langchain.agents import AgentType
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import MessagesPlaceholder, PromptTemplate
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.summarize import load_summarize_chain
-from bs4 import BeautifulSoup
-import requests
 import json
 import openai
-from langchain.schema import SystemMessage
 
 LOG = ForgeLogger(__name__)
 
